98e33eb7026fb7f97df2e71a745f0170471b7d68
赵雷 <7825080@qq.com>
2018-05-23 15:34:38 +0800

    添加一个文件

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,6 @@
 //Route::get('/', function () {
 //    return view('welcome');
 //    App\Http\Controllers\Index123456测试一下密钥;
-//haha    	
+//haha1weqwe    	
 	Route::get('/sort', '\App\Http\Controllers\Index\IndexController@index');
 //});
